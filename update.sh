f8a1e4c12b9c9bd47ac8bea2fbbf4e7918715a11
Gegtor <hektorr112@gmail.com>
2019-06-25 16:27:22 +0200

    first commit

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script installs / updates steamcmd and Unturned 3 on Linux machines
+# Syntax: update.sh
+# Author: fr34kyn01535
+
+STEAMCMD_HOME="$PWD/Steam_CMD"
+UNTURNED_HOME="$PWD/Unturned_Server"
+
+mkdir -p $STEAMCMD_HOME
+mkdir -p $UNTURNED_HOME
+
+cd $STEAMCMD_HOME
+if [ ! -f "steamcmd.sh" ]; then
+	wget http://media.steampowered.com/client/steamcmd_linux.tar.gz
+	tar -xvzf steamcmd_linux.tar.gz
+	rm steamcmd_linux.tar.gz
+fi
+
+./steamcmd.sh +login anonymous +force_install_dir $UNTURNED_HOME +app_update 1110390 validate +exit
+# +@sSteamCmdForcePlatformBitness 32 to use the 32bit version
